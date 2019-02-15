4d12018e2b089a7700dced42b350b238a945f750
Brian Dillingham <bdillingham88@gmail.com>
2019-02-15 08:59:13 -0500

    Remove test route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,4 @@
 <?php
 
-Route::get('test', function(){
-    return 'ok';
-});
-
 Route::get('/{resource}/attachable/{relationship}', '\NovaAttachMany\Http\Controllers\AttachController@create');
 Route::get('/{resource}/{resourceId}/attachable/{relationship}', '\NovaAttachMany\Http\Controllers\AttachController@edit');
