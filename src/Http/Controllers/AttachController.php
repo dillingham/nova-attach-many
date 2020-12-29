477883abfafd4c141b78d8c02f16d981e9a2d267
Alberto Bottarini <alberto.bottarini@gmail.com>
2020-12-29 09:43:28 +0100

    add custom key to manage not-standard belongs2many

--- src/Http/Controllers/AttachController.php
+++ src/Http/Controllers/AttachController.php
@@ -45,7 +45,7 @@ class AttachController extends Controller
 
         return forward_static_call($this->associatableQueryCallable($request, $query), $request, $query)->get()
             ->mapInto($field->resourceClass)
-            ->filter(function ($resource) use ($request, $field, $relationship) {
+            ->filter(function ($resource) use ($request) {
                 return $request->newResource()->authorizedToAttach($request, $resource->resource);
             })->map(function ($resource) use ($request, $field, $resourceClass, $relationship) {
                 return $field->formatAssociatableResource($request, $resource, $resourceClass->newModel(), $relationship);
